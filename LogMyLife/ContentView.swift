3c9e23873f56d6de372221f89309d3fd5a5caaa8
TamasPiros <tomi.piros@gmail.com>
2026-05-02 15:26:11 +0200

    Initial Commit

---
+++ LogMyLife/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LogMyLife
+//
+//  Created by Piros Tamás on 2026. 05. 02..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LogMyLife/LogMyLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogMyLifeApp.swift
+//  LogMyLife
+//
+//  Created by Piros Tamás on 2026. 05. 02..
+//
+
+import SwiftUI
+
+@main
+struct LogMyLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
